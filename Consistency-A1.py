3180ac9c207a8090bdf39841df18e6c84adf194c
Tanish-04 <63210891+Tanish-04@users.noreply.github.com>
2022-08-16 17:27:05 +0500

    Consistency A1 solution completed

---
+++ Consistency-A1.py
@@ -0,0 +1,32 @@
+
+def isVowel(s):
+    if s == 'A' or s == 'E' or s == 'I' or s == 'O' or s == 'U':
+        return True
+
+
+def solution():
+
+    string = input()
+    ans = float('inf')
+    # Outer loop for going through every alphabet to find minimum cost for consistent string
+    for i in range(ord('A'), ord('Z') + 1):
+        cost = 0
+        # Inner loop going through string and if our outer alphabet is same as string do nothing and if it's different then use function
+        # isVowel and add cost 2 if it is vowel and add cost 1 if it is not vowel
+        for var in string:
+            if var != chr(i):
+                if isVowel(var) and isVowel(chr(i)):
+                    cost += 2
+
+                cost += 1
+
+        ans = min(cost, ans)
+
+    return ans
+
+
+if __name__ == "__main__":
+    numberOfbirthdays = int(input())
+
+    for i in range(1, numberOfbirthdays + 1):
+        print("Case #{}: {}".format(i, solution()))
